62213f1de35c85fab9754fd0793f885ace070a46
Dan <dbateman0319@starkstate.net>
2017-07-16 19:47:01 -0500

    Begin testing new methods

--- enumerable.rb
+++ enumerable.rb
@@ -3,6 +3,7 @@ module Enumerable
 		x = 0
 		for x in x..array.length do
 			yield(array[x])
+			x += 1
 		end
 			
 		
@@ -12,6 +13,7 @@ module Enumerable
 		x = 0
 		for x in x..array.length do
 			yield(x, array[x])
+			x += 1
 		end
 	end
 
@@ -22,6 +24,7 @@ module Enumerable
 			if yield(array[x]) == true
 				new_array.push(array[x])
 			end
+			x += 1
 		end
 		return new_array
 	end
@@ -38,6 +41,7 @@ module Enumerable
 			else
 				return false
 			end
+			x += 1
 	end
 
 	def my_any?(array)
@@ -52,6 +56,7 @@ module Enumerable
 			else
 				return false
 			end
+			x += 1
 	end
 
 	def my_none?(array)
@@ -66,18 +71,46 @@ module Enumerable
 			else
 				return false
 			end
+			x += 1
+		end
+
 	end
 
 	def my_count(array)
-
+		x = 0
+		true_value = 0
+		for x in x..array.length do
+			if block_given?
+				if yield(array[x]) == true
+					true_value += 1
+				end
+			end
+			x += 1
+		end
+		return true_value
 	end
 
 	def my_map(array)
-
+		x = 0
+		new_array = []
+		for x in x..array.length do
+			new_array[x] = yield(array[x])
+			x += 1
+		end
+		return new_array
 	end
 
-	def my_inject
-
+	def my_inject(array, num)
+		x = 0
+		
+		for x in x..array.length do
+			total = yield(num, array[x])
+			num = total
+			x += 1
+		end
+		return total
 	end
 
 end
+
+test_array = [1, 2, 3, 4, 5]
